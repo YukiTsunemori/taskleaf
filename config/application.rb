4ea289138d275f4e204ad8ea606c75a49fa870bf
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-05-15 15:37:27 +0900

    Added ja.yml for translator

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module Taskleaf
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/ja.yml').to_s]
   end
 end
