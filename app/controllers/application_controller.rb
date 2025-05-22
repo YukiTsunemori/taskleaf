a7527a8da94b18226a67eb45f57e31d31b30977d
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-05-22 23:13:12 +0900

    Add before-action at app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   helper_method :current_user
+  before_action :login_required
 
   private
     # controller内に定義するpublicなメソッドはルーティングが通っていればアクションとして呼び出される可能性がある。
     # データー取得のためのユーティリティメソッドはprivateにすべきである。
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def login_required
+      redirect_to login_url unless current_user
     end
 end
