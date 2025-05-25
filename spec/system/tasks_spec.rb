0631c8f11806d12bd3dd0dc95510a7471ce95f9d
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-05-25 15:00:35 +0900

    tasks_specのログイン処理の共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,13 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "タスク管理機能", type: :system do
-  describe "一覧表示" do
     let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com") }
     let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com") }
+    let!(:task_a) { FactoryBot.create(:task, name: "最初のタスク", user: user_a) }
     # ↑letでuser_aとuser_bを定義
+
     before do
-      FactoryBot.create(:task, name: "最初のタスク", user: user_a)
-      # ユーザーAのタスクを作るところでuser_aを利用している。
       # ----共通化したいログイン処理----BEGIN
       visit login_path
       fill_in "メールアドレス", with: login_user.email
@@ -16,6 +15,7 @@ RSpec.describe "タスク管理機能", type: :system do
       # -------------END-----------------
     end
 
+  describe "一覧表示機能" do
     context "ユーザーAがログインしている時" do
       let(:login_user) { user_a } # login_userを定義
       it "ユーザーAが作成したタスクが表示される" do
@@ -31,6 +31,20 @@ RSpec.describe "タスク管理機能", type: :system do
       it "ユーザーAが作成したタスクが表示されない" do
         # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認。
         expect(page).to have_no_content "最初のタスク"
+      end
+    end
+    
+    describe "詳細表示機能" do
+      context "ユーザーAがログインしている時" do
+        let(:login_user) { user_a }
+
+        before do
+          visit task_path(task_a)
+        end
+
+        it "ユーザーAが作成したタスクが表示される" do
+          expect(page).to have_content "最初のタスク"
+        end
       end
     end
   end
