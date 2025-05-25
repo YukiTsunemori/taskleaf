d93bbb13d3855e0b64731e396728408820c0025e
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-05-25 14:50:49 +0900

    tasks_specのログイン処理の共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,25 +2,35 @@ require "rails_helper"
 
 RSpec.describe "タスク管理機能", type: :system do
   describe "一覧表示" do
+    let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com") }
+    let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com") }
+    # ↑letでuser_aとuser_bを定義
     before do
-      user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com") # ユーザーAを作成しておく（テストデータの準備）
-      # => buildメソッドをcreateの代わりに使えば、DBに登録する前で止めて未保存のオブジェクトを得ることができます。
-      FactoryBot.create(:task, name: "最初のタスク", user: user_a) # 作成者がユーザーAであるタスクを作成しておく（テストデータの準備）
+      FactoryBot.create(:task, name: "最初のタスク", user: user_a)
+      # ユーザーAのタスクを作るところでuser_aを利用している。
+      # ----共通化したいログイン処理----BEGIN
+      visit login_path
+      fill_in "メールアドレス", with: login_user.email
+      fill_in "パスワード", with: login_user.password
+      click_button "ログインする"
+      # -------------END-----------------
     end
 
     context "ユーザーAがログインしている時" do
-      before do
-        # ユーザーAでログインする
-        visit login_path
-        fill_in "メールアドレス", with: "a@example.com"
-        fill_in "パスワード", with: "password"
-        click_button "ログインする"
-      end
-
+      let(:login_user) { user_a } # login_userを定義
       it "ユーザーAが作成したタスクが表示される" do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
         expect(page).to have_content "最初のタスク"
         # page(画面)に期待するよ、・・することを　　　#あるはずだよね、「最初のタスク」という内容が
+      end
+    end
+
+    context "ユーザーBがログインしている時" do
+      let(:login_user) { user_b } # login_userを定義
+
+      it "ユーザーAが作成したタスクが表示されない" do
+        # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認。
+        expect(page).to have_no_content "最初のタスク"
       end
     end
   end
